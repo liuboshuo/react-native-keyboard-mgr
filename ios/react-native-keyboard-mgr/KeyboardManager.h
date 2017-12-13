4a5b3368edbbc20fd0cdf826006bc32c0d37308a
刘硕 <liushuo@visionet.com.cn>
2017-12-13 13:10:43 +0800

    iOS键盘自适应

---
+++ ios/react-native-keyboard-mgr/KeyboardManager.h
@@ -0,0 +1,15 @@
+//
+//  KeyboardManager.h
+//  react-native-keyboard-mgr
+//
+//  Created by 刘硕 on 2017/12/13.
+//  Copyright © 2017年 刘硕. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "IQKeyboardManager.h"
+
+@interface KeyboardManager : NSObject<RCTBridgeModule>
+
+@end
---
+++ ios/react-native-keyboard-mgr/KeyboardManager.m
@@ -0,0 +1,15 @@
+//
+//  KeyboardManager.m
+//  react-native-keyboard-mgr
+//
+//  Created by 刘硕 on 2017/12/13.
+//  Copyright © 2017年 刘硕. All rights reserved.
+//
+
+#import "KeyboardManager.h"
+
+@implementation KeyboardManager
+
+RCT_EXPORT_MODULE(KeyboardManager);
+
+@end
